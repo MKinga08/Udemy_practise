0b936b11ee50f298a4fb8dff8726f09108c8028d
MKinga08 <dr.m.kinga08@gmail.com>
2023-02-24 13:02:54 +0100

    Loops, getters, setters

--- Udemy/Udemy/Program.cs
+++ Udemy/Udemy/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Tracing;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Udemy
+{
+    class Program
+    {
+        private string name = "Robert";
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public static void Main(string[] args)
+        {
+            LoopExamples();
+        }
+
+
+        public static void LoopExamples()
+        {
+            Console.WriteLine("---------For loop---------");
+            List<string> collection = new List<string>() { "a", "b", "c" };
+            for (int i = 0; i < collection.Count; i++)
+            {
+                Console.WriteLine(collection[i]);
+            }
+            Console.WriteLine("---------while loop---------");
+            int k = 3;
+            while (k > 0)
+            {
+                Console.WriteLine("Loading");
+                k--;
+            }
+
+        }
+
+    }
+}
